226f9a9215b961617f30e3398046256f6ceb4c35
Courela <courela@gmail.com>
2025-06-16 00:24:02 +0100

    Print/render in a separate js file; reprint order page

---
+++ render.js
@@ -0,0 +1,194 @@
+function printMeals(toPrepareMeals) {
+	let domMeals = $("#meals");
+	let table = $('<table></table>');
+	let header = $("<tr><th>Mesa</th><th>Estado</th><th>Item</th></tr>");
+	table.append(header);
+	for (let i = 0; i < toPrepareMeals.length; i++) {
+		let row = $("<tr></tr>");
+		row.append('<td>' + toPrepareMeals[i].table + "</td>");
+		row.append('<td>' + toPrepareMeals[i].status + "</td>");
+        row.append('<td>' + toPrepareMeals[i].itemName + "</td>");
+		row.append('<td>' + toPrepareMeals[i].description + "</td>");
+		
+		table.append(row);
+	}
+	
+	domMeals.append(table);
+}
+
+function printDashboard(mealRequests) {
+	let domDishes = $("#dishes");
+    domDishes.html('');
+
+    if (!window.showOrders) {
+        if (!window.showTables) {
+            printByDish(domDishes);
+        }
+
+        if (window.showTables) {
+            printByTable(domDishes, mealRequests);
+        }
+    } else {
+        printByOrders(domDishes, mealRequests);
+    }
+}
+
+function printByDish(domDishes) {
+    let menus = window.menus;
+    for (let x = 0; x < menus.length; x++) {
+        const categoryItem = menus[x];
+        if (inSelectedAllCategories() || (isSelectedCategory(categoryItem.category) && isSelectedDescription(categoryItem.category, categoryItem.description))) {
+            let div = $('<div id="cell-' + categoryItem.itemId + '" class="board-cell"></div>');
+            let nrDishes = 0;
+            let status = '--';
+            if (categoryItem.dishes.length > 0) {
+                for (let y = 0; y < categoryItem.dishes.length; y++) {
+                    const dish = categoryItem.dishes[y];
+                    if (isSelected(dish)) {
+                        nrDishes++;
+                        if (status === '--') {
+                            status = dish.status;
+                        } else if (status !== 'MULTI' && status !== dish.status) {
+                            status = 'MULTI';
+                        }
+                    }
+                }
+                if (nrDishes > window.dishWarningThreshold) {
+                    div.addClass('warning');
+                }
+            }
+            if (categoryItem.available || nrDishes > 0) {
+                div.append('<span class="top-left tiny">'+status+'</span>');
+                div.append('<span>'+ categoryItem.name +'</span><br />');
+                div.append('<span class="alignBottom">'+ nrDishes.toString() +'</span>');
+                domDishes.append(div);
+            }
+        }
+    }
+}
+
+function printByTableOrdered(domDishes) {
+    /*let menus = window.menus;
+    for (let x = 0; x < menus.length; x++) {
+        const categoryItem = menus[x];
+        if (inSelectedAllCategories() || (isSelectedCategory(categoryItem.category) && isSelectedDescription(categoryItem.category, categoryItem.description))) {
+            console.log('Found selected category: ', categoryItem.category);
+            let div = $('<div id="cell-' + categoryItem.itemId + '" class="board-cell"></div>');
+            let nrDishes = 0;
+            let status = '--';
+            if (categoryItem.dishes.length > 0) {
+                for (let y = 0; y < categoryItem.dishes.length; y++) {
+                    const dish = categoryItem.dishes[y];
+                    if (isSelected(dish)) {
+                        nrDishes++;
+                        if (status === '--') {
+                            status = dish.status;
+                        } else if (status !== 'MULTI' && status !== dish.status) {
+                            status = 'MULTI';
+                        }
+                    }
+                }
+                if (nrDishes > window.dishWarningThreshold) {
+                    div.addClass('warning');
+                }
+            }
+            div.append('<span class="top-left tiny">'+status+'</span>');
+            div.append('<span>'+ categoryItem.name +'</span><br />');
+            div.append('<span class="alignBottom">'+ nrDishes.toString() +'</span>');
+            domDishes.append(div);
+        }
+    }*/
+}
+
+function printByTable(domDishes, mealRequests){
+    let groupByStatuses = groupByStatus(mealRequests, window.descriptionSplit);
+    if (groupByStatuses) {
+        let statuses = Object.keys(groupByStatuses);
+        for (let i = 0; i < statuses.length; i++) {
+            let statusDishList = groupByStatuses[statuses[i]];
+            let statusKeys = Object.keys(statusDishList);
+            for (let keyIndex = 0; keyIndex < statusKeys.length; keyIndex++) {
+                let dishes = statusDishList[statusKeys[keyIndex]].reduce((acc, dish) => {
+                    if (isSelected(dish)) {
+                        const key = dish.itemName;
+                        if (!acc[key]) {
+                            acc[key] = 0;
+                        }
+                        acc[key] = acc[key] + 1;
+                    }
+                    return acc;
+                }, {});
+
+                printCells(keyIndex, dishes, statusDishList, statusKeys, statuses[i], domDishes);
+            }
+        }
+    } else {
+        let div = $('<div class="dynamic">Sem refeições</div>');
+        domDishes.append(div);
+    }
+}
+
+function printCells(idx, dishes, statusDishList, statusKeys, status, domDishes) {
+    // console.log("Dishes: " + JSON.stringify(dishes));
+    let dishNames = Object.keys(dishes);
+    for (let j = 0; j < dishNames.length; j ++) {
+        let div = $('<div class="board-cell"></div>');
+        div.append('<span class="top-left tiny">'+status+'</span>');
+        div.append('<span>'+ dishNames[j] +'</span><br /><span>'+ dishes[dishNames[j]].toString() +'</span>');
+        if (window.showTables) {
+            let tables = getTablesForDish(dishNames[j], statusDishList[statusKeys[idx]]);
+            if (tables) {
+                for (let l = 0; l < tables.length; l++) {
+                    let orderId = $('<input name="' + tables[l].table + '_orderId" type="hidden" value="' + tables[l].orderId + '"></input>')
+                    let itemId = $('<input name="' + tables[l].table + '_itemId" type="hidden" value="' + tables[l].itemId + '"></input>')
+                    //if (status[statusKeys[idx]] === 'ORDERED') {
+                        let link = $('<a href="#">' + tables[l].table + '</a>');
+                        let createdDom = $('<span class="time">' + toDateTime(tables[l].createdAt) + '</span>');
+                        link.on('click', (evt) => {
+                            let tbl = evt.currentTarget.childNodes[0].data;
+                            let ok = confirm('Marcar prato "' + dishNames[j] + '" para a mesa "' + tbl + '" como servido?');
+                            if (ok) {
+                                markAsServed(evt);
+                            }
+                        });
+                        div.append('<br />', orderId, itemId, link, createdDom);
+                    // } else {
+                    //     div.append(orderId, itemId);
+                    // }
+                }
+            }
+        }
+        domDishes.append(div);
+    }
+}
+
+function printByOrders(domDishes, mealRequests) {
+    let groupByTables = groupByTable(mealRequests);
+    let tables = Object.keys(groupByTables);
+    for (let i = 0; i < tables.length; i++) {
+        let table = tables[i];
+        const items = groupByTables[table];
+        let domTable = $('<p></p>');
+        let domTableLink = $('<a href="#">'+ table +'</a>');
+        domTableLink.on('click', (evt) => {
+            let tbl = evt.currentTarget.childNodes[0].data;
+            let req = { "table": tbl, "items": [] };
+            for (let j = 0; j < items.length; j++) {
+                if (isSelected(items[j])) {
+                    let item = req.items.find(it => it.name === items[j].itemName);
+                    if (item) {
+                        item.quantity++; 
+                    } else {
+                        req.items.push({ "quantity": 1, "name": items[j].itemName });
+                    }
+                }
+            }
+            let ok = confirm('Imprimir categorias seleccionadas para a mesa ' + tbl + '? ' );
+            if (ok) {
+                // sendToPrinter();
+            }
+        });
+        domTable.append(domTableLink);
+        domDishes.append(domTable);
+    }
+}
