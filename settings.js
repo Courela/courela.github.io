a61eed141e58bc5589bb89b730a1435c7e306ea2
Courela <courela@gmail.com>
2025-06-18 14:01:07 +0100

    Lower refresh period do 120 sec

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ function setSettings() {
 	window.descriptionSplit = true;
 	window.nullDescription = 'Geral';
 	window.showStatuses = ["ORDERED", "COOKING", "READY", "SERVED", "CANCELLED" ];
-	window.refreshPeriod = 600000;
+	window.refreshPeriod = 120000;
 	window.markedAsStatus = 'SERVED';
 	window.showFullDate = false;
 	window.getMealsBackTo = 6; // hours
