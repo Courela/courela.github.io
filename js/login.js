6dd5192e28ef778283bb54fb0e2605e635840125
Courela <courela@gmail.com>
2025-06-29 22:45:01 +0100

    Back hyperlink

--- js/login.js
+++ js/login.js
@@ -15,20 +15,12 @@ async function login() {
 	let password = $('#password').val();
 	
 	if (username && password) {
-		let url = 'https://api.waiterio.com/api/v3/authentication';
+		let url = window.apiURL + '/authentication';
 		let body = prepareAuthentication(username, password);
 		const response = await authenticate(url, body);
 		if (response) {
-			// let restaurantId = sessionStorage.getItem("restaurantId");
-			// let token = sessionStorage.getItem("token");
 			sessionStorage.setItem("username", username);
-			// displayAll(true);
-			// await print(restaurantId);
 			window.location.href = "links.html";
-			
-			// await refreshAuth(true);
-			// $('#credentials').hide();
-			// $('#links').show();
 			result = true;
 		} else {
 			alert('Autentica\xE7\xE3o falhou!');
