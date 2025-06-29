c886240ededea1323c9d45b0b4da70a18ad62d52
Courela <courela@gmail.com>
2025-06-29 11:10:42 +0100

    Change place of login function to login.js

--- login.js
+++ login.js
@@ -5,3 +5,37 @@ function bindLogin() {
         $('#spinner').hide();
     });
 }
+
+async function login() {	
+	console.log("Going to authenticate...");
+	
+	let result = false;
+
+	let username = $('#username').val();
+	let password = $('#password').val();
+	
+	if (username && password) {
+		let url = 'https://api.waiterio.com/api/v3/authentication';
+		let body = prepareAuthentication(username, password);
+		const response = await authenticate(url, body);
+		if (response) {
+			// let restaurantId = sessionStorage.getItem("restaurantId");
+			// let token = sessionStorage.getItem("token");
+			sessionStorage.setItem("username", username);
+			// displayAll(true);
+			// await print(restaurantId);
+			window.location.href = "links.html";
+			
+			// await refreshAuth(true);
+			// $('#credentials').hide();
+			// $('#links').show();
+			result = true;
+		} else {
+			alert('Autentica\xE7\xE3o falhou!');
+		}
+	} else {
+		alert("Utilizador e Password obrigat\xF3rios");
+	}
+
+	return result;
+}
