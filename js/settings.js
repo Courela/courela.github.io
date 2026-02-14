c0d44dbb90004da776bb34a5d12fd99146a67d12
Courela <courela@gmail.com>
2026-02-14 18:48:19 +0000

    Settings

--- js/settings.js
+++ js/settings.js
@@ -8,7 +8,7 @@ function setSettings() {
 	window.showFullDate = false;
 	window.getMealsBackTo = 3;                    // valid values are (in hours): 1, 3, 6, 12, 24
 	window.dishWarningThreshold = 20;             // show different cell color upon higher requests
-	window.printServerURLOptions = [ '1.101:3000/api', '100.100:3000/api' ];
+	window.printServerURLOptions = [ '1.102:3000/api', '100.100:3000/api' ];
 	window.printServerURL = ''                    // additional printer server
 	window.printerAddrOptions = [ '1.201', '1.202', '1.203', '1.204', '100.201', '100.202', '100.203', '100.204' ];
 	window.printerAddr = '';                      // address of printer to use (printServerURL must be set)
